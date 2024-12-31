a346b3f2580e8bbb387d9a0e536854666c5ae20c
Mayur0018 <mayurnish18@gmail.com>
2024-12-31 08:35:52 +0530

    commit

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
-const { calclulation, x, multiplication,substraction } = require("./calculate/index.js");
-const data = require("./data.json");
-// const { calclulation, x } = require("./calculate/Sum.js");
-// const { multiplication } = require("./calculate/multiplication.js");
-// import { calclulation } from "./Sum.js";
-let a = "Mahyur Nishad";
-calclulation(100, 200);
-console.log(x);
-console.log(a); 
+var name = "You Can Everthing in Your Life"
+console.log(name);
 
-multiplication(10, 20);
-console.log(JSON.stringify(data));
-
-substraction(20,10)
-
-setTimeout(()=>{
-    console.log("i am better Than All");
-    
-},0)
+console.log(global);
+console.log(this);
